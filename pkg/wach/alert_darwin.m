5302b412e87275cd731723ce7c1ca2d2337d040a
Patrick Weppelmann <patrick.weppelmann+KBGroup@neoimpulse.de>
2026-07-29 11:35:16 +0200

    Fix About dialog crash: use CFUserNotification instead of NSAlert (thread-safe)

--- pkg/wach/alert_darwin.m
+++ pkg/wach/alert_darwin.m
@@ -1,17 +1,23 @@
 //go:build darwin
 // +build darwin
 
-#import <Foundation/Foundation.h>
+#import <CoreFoundation/CoreFoundation.h>
 #import <Cocoa/Cocoa.h>
 
+// Thread-safe alert using CoreFoundation (no main thread requirement)
 void showAlert(const char* title, const char* msg) {
-	@autoreleasepool {
-		NSAlert* alert = [[NSAlert alloc] init];
-		alert.messageText = [NSString stringWithUTF8String:title];
-		alert.informativeText = [NSString stringWithUTF8String:msg];
-		[alert addButtonWithTitle:@"OK"];
-		[alert runModal];
-	}
+	CFStringRef t = CFStringCreateWithCString(NULL, title, kCFStringEncodingUTF8);
+	CFStringRef m = CFStringCreateWithCString(NULL, msg, kCFStringEncodingUTF8);
+
+	CFOptionFlags result;
+	CFUserNotificationDisplayAlert(
+		0, kCFUserNotificationNoteAlertLevel,
+		NULL, NULL, NULL,
+		t, m, CFSTR("OK"),
+		NULL, NULL, &result);
+
+	CFRelease(t);
+	CFRelease(m);
 }
 
 void openURL(const char* url) {
